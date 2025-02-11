b076d7c36666aecfcad93e51a61ae56cd2fc0058
jur42282 <jur42282@spseol.cz>
2025-02-11 15:57:26 +0100

    index wip

--- script.js
+++ script.js
@@ -3,7 +3,13 @@ function hideSidebar() {
     sidebar.style.display = 'none';
   }
   
-  function showSidebar() {
+function showSidebar() {
     const sidebar =document.querySelector('.sidebar');
     sidebar.style.display = 'flex';
+  }
+
+function switchTheme() {
+    const body = document.querySelector('body');
+    const currentTheme = body.className;
+    body.className = currentTheme === 'theme-light' ? 'theme-dark' : 'theme-light';
   }
